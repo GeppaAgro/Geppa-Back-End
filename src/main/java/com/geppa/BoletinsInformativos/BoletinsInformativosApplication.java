6dc2f6dc6a3af4d602e0293fe055969abc16047e
Lucas Henrique <96556583+LucasHenriqueSZ@users.noreply.github.com>
2023-10-29 15:33:59 -0300

    feat: Início do Projeto Spring e Configuração do .gitignore

---
+++ src/main/java/com/geppa/BoletinsInformativos/BoletinsInformativosApplication.java
@@ -0,0 +1,13 @@
+package com.geppa.BoletinsInformativos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoletinsInformativosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoletinsInformativosApplication.class, args);
+	}
+
+}
