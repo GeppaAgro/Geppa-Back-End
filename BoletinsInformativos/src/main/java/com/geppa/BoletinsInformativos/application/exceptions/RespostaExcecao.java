65749a85053d6d1bccd634c9ea24f8e19bc20f3c
Lucas Henrique <96556583+LucasHenriqueSZ@users.noreply.github.com>
2023-12-23 18:07:52 -0300

    feat: Ampliado Handler de Exceções para Novos Casos e Refatorado para Utilizar Resposta Padrão
    Adicionado tratamento para novas exceções no handler existente, garantindo a inclusão de novos cenários. Além disso, refatorado o código para adotar a resposta padrão do sistema, proporcionando uma consistência maior nas respostas geradas para diferentes exceções.

--- BoletinsInformativos/src/main/java/com/geppa/BoletinsInformativos/application/exceptions/RespostaExcecao.java
+++
@@ -1,4 +0,0 @@
-package com.geppa.BoletinsInformativos.application.exceptions;
-
-public record RespostaExcecao(String dataHora, int status, String mensagem, String path) {
-}
--- BoletinsInformativos/src/main/java/com/geppa/BoletinsInformativos/application/exceptions/handler/ManipuladorRespostasExcecoes.java
+++ BoletinsInformativos/src/main/java/com/geppa/BoletinsInformativos/application/exceptions/handler/ManipuladorRespostasExcecoes.java
@@ -1,43 +1,42 @@
 package com.geppa.BoletinsInformativos.application.exceptions.handler;
 
-import com.geppa.BoletinsInformativos.application.exceptions.RespostaExcecao;
+import com.geppa.BoletinsInformativos.application.dtos.retorno.RetornoPadraoDto;
 import com.geppa.BoletinsInformativos.domain.exceptions.ConteudoNaoEncontradoExcecao;
 import com.geppa.BoletinsInformativos.domain.exceptions.HashInvalidaExcecao;
+import com.geppa.BoletinsInformativos.domain.exceptions.MapperExcecao;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @ControllerAdvice
 @RestController
 public class ManipuladorRespostasExcecoes extends ResponseEntityExceptionHandler {
 
+    @ExceptionHandler(RuntimeException.class)
+    public final ResponseEntity<RetornoPadraoDto> handleRuntimeException(Exception ex) {
+        RetornoPadraoDto respostaExcecao = new RetornoPadraoDto(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value());
+        return new ResponseEntity<>(respostaExcecao, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
     @ExceptionHandler(HashInvalidaExcecao.class)
-    public final ResponseEntity<RespostaExcecao> handleHashInvalidaExcecao(Exception ex, WebRequest request) {
-        RespostaExcecao respostaExcecao = new RespostaExcecao(getDataHora(), HttpStatus.BAD_REQUEST.value(), ex.getMessage(), getPath(request));
+    public final ResponseEntity<RetornoPadraoDto> handleHashInvalidaExcecao(Exception ex) {
+        RetornoPadraoDto respostaExcecao = new RetornoPadraoDto(ex.getMessage(), HttpStatus.BAD_REQUEST.value());
         return new ResponseEntity<>(respostaExcecao, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(ConteudoNaoEncontradoExcecao.class)
-    public final ResponseEntity<RespostaExcecao> handleConteudoNaoEncontradoExcecao(Exception ex, WebRequest request) {
-        RespostaExcecao respostaExcecao = new RespostaExcecao(getDataHora(), HttpStatus.NOT_FOUND.value(), ex.getMessage(), getPath(request));
+    public final ResponseEntity<RetornoPadraoDto> handleConteudoNaoEncontradoExcecao(Exception ex) {
+        RetornoPadraoDto respostaExcecao = new RetornoPadraoDto(ex.getMessage(), HttpStatus.NOT_FOUND.value());
         return new ResponseEntity<>(respostaExcecao, HttpStatus.NOT_FOUND);
     }
 
-    private String getDataHora() {
-        LocalDateTime momentoAtual = LocalDateTime.now();
-        DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
-        return momentoAtual.format(formatador);
-    }
-
-    private String getPath(WebRequest request) {
-        return request.getDescription(false).split("=")[1];
+    @ExceptionHandler(MapperExcecao.class)
+    public final ResponseEntity<RetornoPadraoDto> handleMapperExcecao(Exception ex) {
+        RetornoPadraoDto respostaExcecao = new RetornoPadraoDto(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value());
+        return new ResponseEntity<>(respostaExcecao, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
