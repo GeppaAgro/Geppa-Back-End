f442b14d72766399627227c6fb989d878bd48eca
Lucas Henrique <96556583+LucasHenriqueSZ@users.noreply.github.com>
2024-02-08 23:03:01 -0300

    feat: Adicionada Classe DTO para Representar Filtro Genérico com Filtros de Data e Tags
    Implementada uma classe DTO para representar um filtro genérico, incluindo filtros para campos de data e tags. Essa adição proporciona uma estrutura padronizada para realizar consultas filtradas com base em critérios de data e tags em diferentes partes do sistema.

---
+++ BoletinsInformativos/src/main/java/com/geppa/BoletinsInformativos/application/dtos/filters/FiltroGenericoDto.java
@@ -0,0 +1,40 @@
+package com.geppa.BoletinsInformativos.application.dtos.filters;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class FiltroGenericoDto {
+    private LocalDate dataMinima;
+    private LocalDate dataMaxima;
+    private List<String> tags;
+
+    public FiltroGenericoDto(LocalDate dataMinima, LocalDate dataMaxima, List<String> tags) {
+        this.dataMinima = dataMinima;
+        this.dataMaxima = dataMaxima;
+        this.tags = tags;
+    }
+
+    public LocalDate getDataMinima() {
+        return dataMinima;
+    }
+
+    public LocalDate getDataMaxima() {
+        return dataMaxima;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setDataMinima(LocalDate dataMinima) {
+        this.dataMinima = dataMinima;
+    }
+
+    public void setDataMaxima(LocalDate dataMaxima) {
+        this.dataMaxima = dataMaxima;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+}
