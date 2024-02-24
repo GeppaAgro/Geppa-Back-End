c149d14ddd6bff01c5c39eaf52d5ec15bb51f7b4
Lucas Henrique <96556583+LucasHenriqueSZ@users.noreply.github.com>
2024-02-24 11:34:57 -0300

    feat: Implementado Métodos de Salvamento e Busca de Registros Duplicados no Gateway Genérico do Repositório
    Foram implementados métodos para salvar e buscar registros duplicados no gateway genérico do repositório. Esses métodos realizam a conversão de entidade para o modelo e executam as operações no banco de dados utilizando o repository injetado.

--- BoletinsInformativos/src/main/java/com/geppa/BoletinsInformativos/infrastructure/gateways/GatewayGenericoRepositorio.java
+++ BoletinsInformativos/src/main/java/com/geppa/BoletinsInformativos/infrastructure/gateways/GatewayGenericoRepositorio.java
@@ -5,7 +5,7 @@ import com.geppa.BoletinsInformativos.domain.exceptions.MapperExcecao;
 import com.geppa.BoletinsInformativos.domain.exceptions.OrdenacaaoInvalidaExcecao;
 import com.geppa.BoletinsInformativos.infrastructure.persistencia.RepositorioGenerico;
 import com.geppa.BoletinsInformativos.util.mapper.Mapper;
-import com.geppa.BoletinsInformativos.util.messages.MensagensExcecao;
+import com.geppa.BoletinsInformativos.util.enums.messages.MensagensExcecao;
 import org.springframework.data.domain.Page;
 
 import org.springframework.data.domain.Pageable;
@@ -37,8 +37,32 @@ public class GatewayGenericoRepositorio<T> {
     }
 
     public Page<T> buscarTodosComPaginacaoEFiltro(Pageable pageable, Specification<T> filtro) {
-        try{
-        return repository.findAll(filtro, pageable)
+        try {
+            return repository.findAll(filtro, pageable)
+                    .map(contentModel -> {
+                        try {
+                            return Mapper.parseObject(contentModel, entityType);
+                        } catch (Exception e) {
+                            throw new MapperExcecao(MensagensExcecao.FALHA_CONVERSAO_MODELO_DOMINIO.getMensagem());
+                        }
+                    });
+        } catch (PropertyReferenceException e) {
+            throw new OrdenacaaoInvalidaExcecao(pageable.getSort().toString());
+        } catch (Exception e) {
+            throw new BuscaInvalidaExcecao();
+        }
+    }
+
+    public T salvar(T objeto) {
+        try {
+            return Mapper.parseObject(repository.save(Mapper.parseObject(objeto, repository.getClasseModelo())), entityType);
+        } catch (Exception e) {
+            throw new MapperExcecao(MensagensExcecao.FALHA_CONVERSAO_MODELO_DOMINIO.getMensagem());
+        }
+    }
+
+    public Optional<T> buscarDuplicado(T objeto) {
+        return repository.buscarDuplicado(Mapper.parseObject(objeto, repository.getClasseModelo()))
                 .map(contentModel -> {
                     try {
                         return Mapper.parseObject(contentModel, entityType);
@@ -46,10 +70,7 @@ public class GatewayGenericoRepositorio<T> {
                         throw new MapperExcecao(MensagensExcecao.FALHA_CONVERSAO_MODELO_DOMINIO.getMensagem());
                     }
                 });
-        } catch (PropertyReferenceException e) {
-            throw new OrdenacaaoInvalidaExcecao(pageable.getSort().toString());
-        } catch (Exception e) {
-            throw new BuscaInvalidaExcecao();
-        }
     }
+
+
 }
