0980d36a74b4651339afbfe13410733300cb2032
Lucas Henrique <96556583+LucasHenriqueSZ@users.noreply.github.com>
2024-02-24 11:07:30 -0300

    feat: Adicionadas DTOs de Cadastro e Retorno para Inscrição na Newsletter
    Foram implementadas as DTOs de cadastro e retorno para facilitar a comunicação e transferência de dados relacionados à inscrição na newsletter.

---
+++ BoletinsInformativos/src/main/java/com/geppa/BoletinsInformativos/application/dtos/cadastro/InscricaoNewsletterCadastroDto.java
@@ -0,0 +1,17 @@
+package com.geppa.BoletinsInformativos.application.dtos.cadastro;
+
+public class InscricaoNewsletterCadastroDto {
+
+    private String email;
+
+    public InscricaoNewsletterCadastroDto() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
