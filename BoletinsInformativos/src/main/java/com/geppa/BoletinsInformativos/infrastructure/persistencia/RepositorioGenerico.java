de064a7add140d02815a1bcf56713d3202c7549c
Lucas Henrique <96556583+LucasHenriqueSZ@users.noreply.github.com>
2024-02-24 11:11:04 -0300

    feat: Adicionado Método para Retornar Classe de Modelo e Verificar Registros Duplicados no Repositório Genérico
    Implementado método no repositório genérico para retornar a classe de modelo e verificar a existência de registros duplicados. Esta adição permite uma verificação mais abrangente dos dados e facilita a detecção de registros duplicados no sistema.

--- BoletinsInformativos/src/main/java/com/geppa/BoletinsInformativos/infrastructure/persistencia/RepositorioGenerico.java
+++ BoletinsInformativos/src/main/java/com/geppa/BoletinsInformativos/infrastructure/persistencia/RepositorioGenerico.java
@@ -4,8 +4,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.NoRepositoryBean;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @NoRepositoryBean
 public interface RepositorioGenerico<T> extends JpaRepository<T, UUID>, JpaSpecificationExecutor<T> {
+    default Class<T> getClasseModelo() {
+        throw new UnsupportedOperationException("Método não implementado");
+    }
+
+    default Optional<T> buscarDuplicado(T objeto) {
+        throw new UnsupportedOperationException("Método não implementado");
+    }
 }
