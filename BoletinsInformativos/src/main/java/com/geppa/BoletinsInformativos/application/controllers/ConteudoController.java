fece9dfe9e48ba6657801204d06cc567ad2ec705
Lucas Henrique <96556583+LucasHenriqueSZ@users.noreply.github.com>
2024-03-04 21:59:30 -0300

    feat: Adicionado DTO para Últimos Conteúdos Cadastrados e Controller com Endpoint de Busca
    Implementado DTO para representar os últimos conteúdos cadastrados e criada a controller com o endpoint para buscar esses últimos conteúdos cadastrados. Essa adição facilita o acesso e a exibição dos conteúdos mais recentes na aplicação.

---
+++ BoletinsInformativos/src/main/java/com/geppa/BoletinsInformativos/application/controllers/ConteudoController.java
@@ -0,0 +1,45 @@
+package com.geppa.BoletinsInformativos.application.controllers;
+
+import com.geppa.BoletinsInformativos.application.dtos.padrao.RetornoPadraoDto;
+import com.geppa.BoletinsInformativos.application.dtos.retorno.UltimoConteudoDto;
+import com.geppa.BoletinsInformativos.domain.classes.conteudos.Conteudo;
+import com.geppa.BoletinsInformativos.domain.useCases.ultimosConteudos.UltimosConteudosPorTipoConteudo;
+import com.geppa.BoletinsInformativos.util.mapper.Mapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+
+@RestController
+@RequestMapping("/conteudos")
+public class ConteudoController {
+
+    private final UltimosConteudosPorTipoConteudo ultimosConteudosPorTipoConteudo;
+
+    public ConteudoController(UltimosConteudosPorTipoConteudo ultimosConteudosPorTipoConteudo) {
+        this.ultimosConteudosPorTipoConteudo = ultimosConteudosPorTipoConteudo;
+    }
+
+    @GetMapping("/ultimos-por-conteudo")
+    public ResponseEntity<RetornoPadraoDto> getUltimosConteudosPorConteudo(@RequestParam int quantidade) {
+
+        HashMap<String, List<Conteudo>> ultimosConteudos = ultimosConteudosPorTipoConteudo.executar(quantidade);
+
+        HashMap<String, List< UltimoConteudoDto>> ultimosConteudosDto = new HashMap<>();
+        ultimosConteudos.forEach((k, v) -> {
+          ultimosConteudosDto.put(k, Mapper.parseListObjects(v, UltimoConteudoDto.class));
+        });
+
+        RetornoPadraoDto retornoSucessoDto = new RetornoPadraoDto(
+                "Ultimos conteúdos encontrados com sucesso",
+                200,
+                ultimosConteudosDto
+        );
+
+        return ResponseEntity.ok(retornoSucessoDto);
+    }
+}
