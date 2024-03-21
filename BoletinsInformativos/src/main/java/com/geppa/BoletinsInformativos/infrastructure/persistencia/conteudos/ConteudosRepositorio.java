05cfd41437af7c7eee0590403e462989908c77dc
Lucas Henrique <96556583+LucasHenriqueSZ@users.noreply.github.com>
2024-03-21 19:17:40 -0300

    feat: adicionado Repository para conteudos

---
+++ BoletinsInformativos/src/main/java/com/geppa/BoletinsInformativos/infrastructure/persistencia/conteudos/ConteudosRepositorio.java
@@ -0,0 +1,10 @@
+package com.geppa.BoletinsInformativos.infrastructure.persistencia.conteudos;
+
+import com.geppa.BoletinsInformativos.infrastructure.model.conteudos.ConteudoModel;
+import com.geppa.BoletinsInformativos.infrastructure.persistencia.RepositorioGenerico;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ConteudosRepositorio extends RepositorioGenerico<ConteudoModel> {
+
+}
