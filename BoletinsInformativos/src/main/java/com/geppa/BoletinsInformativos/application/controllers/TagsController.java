11f918d7c0d4120ed8cf3bc4198430ef53a4a3b3
Lucas Henrique <96556583+LucasHenriqueSZ@users.noreply.github.com>
2024-04-02 17:59:19 -0300

    feat: Adicionado Método de Consulta Paginada de Todas as Tags à Controller de Tag
    Implementado o método na controller de tag para permitir a consulta paginada de todas as tags.

--- BoletinsInformativos/src/main/java/com/geppa/BoletinsInformativos/application/controllers/TagsController.java
+++ BoletinsInformativos/src/main/java/com/geppa/BoletinsInformativos/application/controllers/TagsController.java
@@ -1,17 +1,21 @@
 package com.geppa.BoletinsInformativos.application.controllers;
 
+import com.geppa.BoletinsInformativos.application.dtos.padrao.RetornoPadraoComPaginacaoDto;
 import com.geppa.BoletinsInformativos.application.dtos.padrao.RetornoPadraoDto;
 import com.geppa.BoletinsInformativos.application.dtos.retorno.TagDto;
+import com.geppa.BoletinsInformativos.application.hateoas.HateoasPaginacao;
 import com.geppa.BoletinsInformativos.domain.classes.Tag;
+import com.geppa.BoletinsInformativos.domain.useCases.genericos.ConsultarTodos;
 import com.geppa.BoletinsInformativos.domain.useCases.tags.BuscarTagPorNome;
 import com.geppa.BoletinsInformativos.util.enums.messages.MensagensRetorno;
 import com.geppa.BoletinsInformativos.util.mapper.Mapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,9 +24,11 @@ import java.util.List;
 public class TagsController {
 
     private final BuscarTagPorNome buscarPorNome;
+    private final ConsultarTodos consultarTodos;
 
-    public TagsController(BuscarTagPorNome buscarPorNome) {
+    public TagsController(BuscarTagPorNome buscarPorNome, ConsultarTodos consultarTodos) {
         this.buscarPorNome = buscarPorNome;
+        this.consultarTodos = consultarTodos;
     }
 
     @GetMapping("/buscarPorNome")
@@ -34,6 +40,28 @@ public class TagsController {
                 HttpStatus.OK.value(),
                 Mapper.parseListObjects(tags, TagDto.class)
         ));
+    }
+
+    @GetMapping
+    public ResponseEntity<RetornoPadraoComPaginacaoDto> buscarTodos(
+            @PageableDefault(sort = "nome", direction = Sort.Direction.DESC) Pageable pageable) {
+
+        Page<Tag> tags = consultarTodos.executar(pageable, Tag.class);
+
+        RetornoPadraoComPaginacaoDto retornoSucessoDto = new RetornoPadraoComPaginacaoDto(
+                MensagensRetorno.BUSCA_REALIZADA_COM_SUCESSO.getMensagem(),
+                HttpStatus.OK.value(),
+                Mapper.parseListObjects(tags.getContent(), TagDto.class),
+                tags.getPageable().getPageNumber(),
+                tags.getSize(),
+                tags.getTotalElements(),
+                tags.getTotalPages(),
+                pageable.getSort().toString()
+        );
+
+        HateoasPaginacao.addHateoas(retornoSucessoDto, tags);
+
+        return ResponseEntity.ok(retornoSucessoDto);
     }
 
 }
