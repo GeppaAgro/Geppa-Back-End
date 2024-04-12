6f0f17c0bbc8723f64b6dd125304b0c4acfe4cb3
Lucas Henrique <96556583+LucasHenriqueSZ@users.noreply.github.com>
2024-04-11 21:05:51 -0300

    feat: Adicionado Endpoint na Controller de Tag para Deleção de Tags
    Implementado um novo endpoint na controller de tag para permitir a deleção de tags. Agora os usuários podem remover tags através da API, oferecendo uma funcionalidade essencial para a gestão de tags na aplicação.

--- BoletinsInformativos/src/main/java/com/geppa/BoletinsInformativos/application/controllers/TagsController.java
+++ BoletinsInformativos/src/main/java/com/geppa/BoletinsInformativos/application/controllers/TagsController.java
@@ -12,6 +12,7 @@ import com.geppa.BoletinsInformativos.domain.classes.Tag;
 import com.geppa.BoletinsInformativos.domain.useCases.genericos.Cadastrar;
 import com.geppa.BoletinsInformativos.domain.useCases.genericos.ConsultarTodos;
 import com.geppa.BoletinsInformativos.domain.useCases.tags.BuscarTagPorNome;
+import com.geppa.BoletinsInformativos.domain.useCases.tags.DeletarTag;
 import com.geppa.BoletinsInformativos.util.enums.messages.MensagensRetorno;
 import com.geppa.BoletinsInformativos.util.mapper.Mapper;
 import org.springframework.data.domain.Page;
@@ -32,17 +33,19 @@ public class TagsController {
 
     private final BuscarTagPorNome buscarPorNome;
     private final ConsultarTodos consultarTodos;
+    private final DeletarTag deletarTag;
     private final Cadastrar cadastrar;
 
-    public TagsController(BuscarTagPorNome buscarPorNome, ConsultarTodos consultarTodos, Cadastrar cadastrar) {
+    public TagsController(BuscarTagPorNome buscarPorNome, ConsultarTodos consultarTodos, Cadastrar cadastrar, DeletarTag deletarTag){
         this.buscarPorNome = buscarPorNome;
         this.consultarTodos = consultarTodos;
         this.cadastrar = cadastrar;
+        this.deletarTag = deletarTag;
     }
 
-    @GetMapping("/buscarPorNome")
-    public ResponseEntity<RetornoPadraoDto> buscarPorNome(@RequestParam String nomeTag) {
-        List<Tag> tags = buscarPorNome.executar(nomeTag);
+    @GetMapping("/{nome}")
+    public ResponseEntity<RetornoPadraoDto> buscarPorNome(@PathVariable String nome) {
+        List<Tag> tags = buscarPorNome.executar(nome);
 
         return ResponseEntity.ok(new RetornoPadraoDto(
                 MensagensRetorno.BUSCA_REALIZADA_COM_SUCESSO.getMensagem(),
@@ -82,6 +85,16 @@ public class TagsController {
 
         URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(tag.getId()).toUri();
         return ResponseEntity.created(location).body(resposta);
+    }
+
+    @DeleteMapping("/{nome}")
+    public ResponseEntity<RetornoPadraoDto> deletar(@PathVariable String nome) {
+        deletarTag.executar(nome);
+
+        return ResponseEntity.ok(new RetornoPadraoDto(
+                MensagensRetorno.TAG_DELETADA_COM_SUCESSO.getMensagem(),
+                HttpStatus.OK.value()
+        ));
     }
 
 }
