9041d3b9f53344f3ea433058fd5f3fec96543a50
Lucas Henrique <96556583+LucasHenriqueSZ@users.noreply.github.com>
2024-05-04 18:42:53 -0300

    fix: corrige erros de validaação dos conteudos de artigo e mensagens de validação

--- BoletinsInformativos/src/main/java/com/geppa/BoletinsInformativos/domain/classes/conteudos/Artigo.java
+++ BoletinsInformativos/src/main/java/com/geppa/BoletinsInformativos/domain/classes/conteudos/Artigo.java
@@ -1,8 +1,10 @@
 package com.geppa.BoletinsInformativos.domain.classes.conteudos;
 
 import com.geppa.BoletinsInformativos.domain.classes.Autor;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
@@ -12,11 +14,12 @@ import java.util.List;
 public class Artigo extends Conteudo {
 
     @NotNull(message = "{artigo.dataPublicacao.notNull}")
-    @FutureOrPresent(message = "{artigo.dataPublicacao.futureOrPresent}")
+    @PastOrPresent(message = "{artigo.dataPublicacao.pastOrPresent}")
     private LocalDate dataPublicacao;
 
     @NotNull(message = "{artigo.autores.notNull}")
     @Size(min = 1, message = "{artigo.autores.size}")
+    @Valid
     private List<Autor> autores = new ArrayList<>();
 
     public LocalDate getDataPublicacao() {
