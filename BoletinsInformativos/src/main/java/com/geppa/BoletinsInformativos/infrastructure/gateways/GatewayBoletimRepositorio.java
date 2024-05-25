fcc744680214ee2ce746f3d5d13f8bda50e5ffea
Lucas Henrique <96556583+LucasHenriqueSZ@users.noreply.github.com>
2024-05-25 16:06:10 -0300

    feat: implementado ao gateway do repositorio do boletim o metodo de salvamento de boletins informativos

--- BoletinsInformativos/src/main/java/com/geppa/BoletinsInformativos/infrastructure/gateways/GatewayBoletimRepositorio.java
+++ BoletinsInformativos/src/main/java/com/geppa/BoletinsInformativos/infrastructure/gateways/GatewayBoletimRepositorio.java
@@ -2,20 +2,37 @@ package com.geppa.BoletinsInformativos.infrastructure.gateways;
 
 import com.geppa.BoletinsInformativos.domain.classes.BoletimInformativo;
 import com.geppa.BoletinsInformativos.domain.exceptions.MapperExcecao;
+import com.geppa.BoletinsInformativos.domain.exceptions.ValidacaoTagNaoEncontradaExcecao;
+import com.geppa.BoletinsInformativos.infrastructure.model.BoletimInformativoModel;
+import com.geppa.BoletinsInformativos.infrastructure.model.IndicadorModel;
+import com.geppa.BoletinsInformativos.infrastructure.model.TagModel;
+import com.geppa.BoletinsInformativos.infrastructure.model.conteudos.*;
 import com.geppa.BoletinsInformativos.infrastructure.persistencia.BoletimInformativoRepositorio;
+import com.geppa.BoletinsInformativos.infrastructure.persistencia.ConteudoRepositorio;
+import com.geppa.BoletinsInformativos.infrastructure.persistencia.IndicadoresRepositorio;
+import com.geppa.BoletinsInformativos.infrastructure.persistencia.TagRepositorio;
 import com.geppa.BoletinsInformativos.util.mapper.Mapper;
 import com.geppa.BoletinsInformativos.util.enums.messages.MensagensExcecao;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class GatewayBoletimRepositorio {
 
     private final BoletimInformativoRepositorio boletimInformativoRepositorio;
+    private final TagRepositorio tagRepositorio;
+    private final IndicadoresRepositorio indicadorRepositorio;
+    private final ConteudoRepositorio conteudoRepositorio;
 
-    public GatewayBoletimRepositorio(BoletimInformativoRepositorio boletimInformativoRepositorio) {
+    public GatewayBoletimRepositorio(BoletimInformativoRepositorio boletimInformativoRepositorio,
+                                     TagRepositorio tagRepositorio,
+                                     IndicadoresRepositorio indicadorRepositorio, ConteudoRepositorio conteudoRepositorio) {
         this.boletimInformativoRepositorio = boletimInformativoRepositorio;
+        this.tagRepositorio = tagRepositorio;
+        this.indicadorRepositorio = indicadorRepositorio;
+        this.conteudoRepositorio = conteudoRepositorio;
     }
 
     public Optional<BoletimInformativo> buscarPorEdicao(String edicao) {
@@ -29,4 +46,48 @@ public class GatewayBoletimRepositorio {
                 });
     }
 
+    public BoletimInformativo salvar(BoletimInformativo boletimInformativo) {
+        BoletimInformativoModel boletimInformativoModel = Mapper.parseObject(boletimInformativo, BoletimInformativoModel.class);
+        associarTagsConteudos(boletimInformativoModel);
+
+        BoletimInformativoModel boletimInformativoSalvo = boletimInformativoRepositorio.save(boletimInformativoModel);
+
+        associarConteudos(boletimInformativoSalvo);
+        associarIndicadores(boletimInformativoSalvo);
+
+        return Mapper.parseObject(boletimInformativoSalvo, BoletimInformativo.class);
+    }
+
+
+    private void associarTagsConteudos(BoletimInformativoModel boletimInformativo) {
+        List<ConteudoModel> conteudos = boletimInformativo.getConteudos();
+        for (ConteudoModel conteudo : conteudos) {
+            List<TagModel> tags = conteudo.getTags();
+            for (TagModel tag : tags) {
+                List<TagModel> tagsEncontradas = tagRepositorio.consultarPorNome(tag.getNome().toLowerCase());
+                if (tagsEncontradas.isEmpty()) {
+                    throw new ValidacaoTagNaoEncontradaExcecao(tag.getNome());
+                }
+                TagModel tagComAssociacao = tagsEncontradas.get(0);
+                conteudo.substituirInstanciaTag(tag, tagComAssociacao);
+            }
+        }
+    }
+
+    private void associarIndicadores(BoletimInformativoModel boletimInformativoModel) {
+        List<IndicadorModel> indicadoresParaAssociar = boletimInformativoModel.getIndicadores();
+        for (IndicadorModel indicador : indicadoresParaAssociar) {
+            indicador.setBoletimInformativo(boletimInformativoModel);
+            indicadorRepositorio.save(indicador);
+        }
+    }
+
+    private void associarConteudos(BoletimInformativoModel boletimInformativoModel) {
+        List<ConteudoModel> conteudosParaAssociar = boletimInformativoModel.getConteudos();
+        for (ConteudoModel conteudo : conteudosParaAssociar) {
+            conteudo.setBoletimInformativo(boletimInformativoModel);
+            conteudoRepositorio.save(conteudo);
+        }
+    }
 }
+
