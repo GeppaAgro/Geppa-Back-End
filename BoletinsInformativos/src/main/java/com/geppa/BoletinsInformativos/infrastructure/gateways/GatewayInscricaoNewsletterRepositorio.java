e5828512415659faaaf65cdcf642b9f7f333741a
Lucas Henrique <96556583+LucasHenriqueSZ@users.noreply.github.com>
2024-05-26 17:03:46 -0300

    feat: implementado ao repository busca por emails ativos com paginação

--- BoletinsInformativos/src/main/java/com/geppa/BoletinsInformativos/infrastructure/gateways/GatewayInscricaoNewsletterRepositorio.java
+++ BoletinsInformativos/src/main/java/com/geppa/BoletinsInformativos/infrastructure/gateways/GatewayInscricaoNewsletterRepositorio.java
@@ -1,13 +1,13 @@
 package com.geppa.BoletinsInformativos.infrastructure.gateways;
 
-import com.geppa.BoletinsInformativos.domain.classes.BoletimInformativo;
 import com.geppa.BoletinsInformativos.domain.classes.InscricaoNewsletter;
 import com.geppa.BoletinsInformativos.domain.exceptions.MapperExcecao;
 import com.geppa.BoletinsInformativos.infrastructure.model.InscricaoNewsletterModel;
-import com.geppa.BoletinsInformativos.infrastructure.persistencia.BoletimInformativoRepositorio;
 import com.geppa.BoletinsInformativos.infrastructure.persistencia.InscricaoNewsletterRepositorio;
 import com.geppa.BoletinsInformativos.util.enums.messages.MensagensExcecao;
 import com.geppa.BoletinsInformativos.util.mapper.Mapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -38,5 +38,16 @@ public class GatewayInscricaoNewsletterRepositorio {
         } catch (Exception e) {
             throw new MapperExcecao(MensagensExcecao.FALHA_CONVERSAO_MODELO_DOMINIO.getMensagem());
         }
+    }
+
+    public Page<InscricaoNewsletter> buscarAtivos(Pageable pageable) {
+        Page<InscricaoNewsletterModel> modelos = inscricaoNewsletterRepositorio.findByAtivoTrue(pageable);
+        return modelos.map(modelo -> {
+            try {
+                return Mapper.parseObject(modelo, InscricaoNewsletter.class);
+            } catch (Exception e) {
+                throw new RuntimeException("Falha na conversão de modelo para domínio");
+            }
+        });
     }
 }
