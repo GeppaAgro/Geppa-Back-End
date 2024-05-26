83ec03077b884be988e560ddcc285cc3637afc65
Lucas Henrique <96556583+LucasHenriqueSZ@users.noreply.github.com>
2024-05-26 17:01:57 -0300

    feat: habilita a utilização de requisições assincronas

---
+++ BoletinsInformativos/src/main/java/com/geppa/BoletinsInformativos/infrastructure/config/AsyncConfig.java
@@ -0,0 +1,9 @@
+package com.geppa.BoletinsInformativos.infrastructure.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+}
