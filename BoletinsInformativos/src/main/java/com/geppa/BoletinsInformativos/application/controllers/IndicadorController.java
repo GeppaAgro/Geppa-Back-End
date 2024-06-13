14efeb22f367c7f8193c0b2c7668eafaa14d388c
Lucas Henrique <96556583+LucasHenriqueSZ@users.noreply.github.com>
2024-06-13 00:05:53 -0300

    feat: Adicionado novos endpoints para o gerenciamento de indicadores

--- BoletinsInformativos/src/main/java/com/geppa/BoletinsInformativos/application/controllers/IndicadorController.java
+++ BoletinsInformativos/src/main/java/com/geppa/BoletinsInformativos/application/controllers/IndicadorController.java
@@ -2,25 +2,29 @@ package com.geppa.BoletinsInformativos.application.controllers;
 
 import com.geppa.BoletinsInformativos.application.dtos.cadastro.IndicadorCadastroDto;
 import com.geppa.BoletinsInformativos.application.dtos.padrao.RetornoPadraoDto;
+import com.geppa.BoletinsInformativos.application.dtos.retorno.IndicadorDto;
 import com.geppa.BoletinsInformativos.domain.classes.Indicador;
+import com.geppa.BoletinsInformativos.domain.useCases.Indicador.AtualizarIndicador;
+import com.geppa.BoletinsInformativos.domain.useCases.Indicador.DeletarIndicador;
 import com.geppa.BoletinsInformativos.domain.useCases.genericos.ValidarObjeto;
 import com.geppa.BoletinsInformativos.util.enums.messages.MensagensRetorno;
 import com.geppa.BoletinsInformativos.util.mapper.Mapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/indicadores")
 public class IndicadorController {
 
     private final ValidarObjeto<Indicador> validarIndicador;
+    private final AtualizarIndicador atualizarIndicador;
+    private final DeletarIndicador deletarIndicador;
 
-    public IndicadorController(ValidarObjeto<Indicador> validarIndicador) {
+    public IndicadorController(ValidarObjeto<Indicador> validarIndicador, AtualizarIndicador atualizarIndicador, DeletarIndicador deletarIndicador) {
         this.validarIndicador = validarIndicador;
+        this.atualizarIndicador = atualizarIndicador;
+        this.deletarIndicador = deletarIndicador;
     }
 
     @PostMapping("/validar")
@@ -28,5 +32,22 @@ public class IndicadorController {
         Indicador indicador = Mapper.parseObject(indicadorCadastroDto, Indicador.class);
         validarIndicador.executar(indicador);
         return ResponseEntity.ok(new RetornoPadraoDto(MensagensRetorno.INDICADOR_VALIDADO_COM_SUCESSO.getMensagem(), HttpStatus.OK.value()));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<RetornoPadraoDto> atualizarIndicador(@PathVariable String id,
+                                                               @RequestBody IndicadorCadastroDto indicadorCadastroDto) {
+        Indicador indicador = Mapper.parseObject(indicadorCadastroDto, Indicador.class);
+        Indicador indicadorAtualizado = atualizarIndicador.executar(id, indicador);
+        IndicadorDto indicadorDto = Mapper.parseObject(indicadorAtualizado, IndicadorDto.class);
+        return ResponseEntity.ok(new RetornoPadraoDto(MensagensRetorno.INDICADOR_ATUALIZADO_COM_SUCESSO.getMensagem(),
+                HttpStatus.OK.value(), indicadorDto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<RetornoPadraoDto> deletarIndicador(@PathVariable String id) {
+        deletarIndicador.executar(id);
+        return ResponseEntity.ok(new RetornoPadraoDto(MensagensRetorno.INDICADOR_DELETADO_COM_SUCESSO.getMensagem(),
+                HttpStatus.OK.value()));
     }
 }
